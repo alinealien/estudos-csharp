775c6786cbf418c5c6aaa424464bd3b90d26e341
alinealien <alinebdantunes@gmail.com>
2022-08-28 22:02:46 -0300

    Aula sobre namespace e class Pessoa

--- sintaxe-csharp/Program.cs
+++ sintaxe-csharp/Program.cs
@@ -1,4 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Porque tudo tem um começo... ");
+﻿//Classe - Csharp - AULA I
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Porque tudo tem um começo... ");
 
-//Classe - Csharp
+// AULA sobre namespace: Caminho para a classe
+// É atravez do namespace que ele vai reconhecer a classe [Pessoa] - using [endereço do namespace da classe]
+// Podemos ter duas classes com o mesmo nome, mas, elas devem ter namespaces diferentes
+
+using sintaxe_csharp.Models;
+
+Pessoa p1 = new Pessoa(); //instanciamos
+
+
+p1.Nome = "Aline";
+p1.Idade = 30;
+p1.Apresentar();
